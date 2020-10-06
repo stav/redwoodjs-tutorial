52796e6b74b82dc919582897eeb781dcd0b7d688
Steven Almeroth <sroth77@gmail.com>
2020-10-06 14:12:12 -0400

    workflow

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -5,6 +5,19 @@ import BlogPostsCell from 'src/components/BlogPostsCell'
 const HomePage = () => {
   return (
     <BlogLayout>
+      <p>To sum up, what did we actually do to get this far?</p>
+      <ul>
+        <li>Generate the homepage</li>
+        <li>Generate the blog layout</li>
+        <li>Define the database schema</li>
+        <li>Run migrations to update the database and create a table</li>
+        <li>Scaffold a CRUD interface to the database table</li>
+        <li>
+          Create a cell to load the data and take care of
+          loading/empty/failure/success states
+        </li>
+        <li>Add the cell to the page</li>
+      </ul>
       <BlogPostsCell />
     </BlogLayout>
   )
