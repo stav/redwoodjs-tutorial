2202b67cd33d16cd32d05de02f4ee2fb425df501
Steven Almeroth <sroth77@gmail.com>
2020-10-06 15:51:10 -0400

    post detail

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -18,6 +18,27 @@ const HomePage = () => {
         </li>
         <li>Add the cell to the page</li>
       </ul>
+      <p> Then </p>
+      <ol>
+        <li>
+          We created a new page to show a single post (the &quot;detail&quot;
+          page).
+        </li>
+        <li>
+          We added a route to handle the <code>id</code> of the post and turn it
+          into a route param.
+        </li>
+        <li>We created a cell to fetch and display the post.</li>
+        <li>
+          Redwood made the world a better place by making that <code>id</code>
+          available to us at several key junctions in our code and even turning
+          it into a number automatically.
+        </li>
+        <li>
+          We turned the actual post display into a standard React component and
+          used it in both the homepage and new detail page.
+        </li>
+      </ol>
       <BlogPostsCell />
     </BlogLayout>
   )
